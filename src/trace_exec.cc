8654fbbc39dda4bbf16983483485237c8aa6f77a
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2024-12-26 21:53:47 +0000

    fix open syscall handling when O_DIRECTORY

--- src/trace_exec.cc
+++ src/trace_exec.cc
@@ -960,18 +960,18 @@ int run_tracer(pid_t child_pid)
           access_type = (flags & O_WRONLY || flags & O_RDWR) ? 'W' : 'R';
           file_type = (flags & O_DIRECTORY) ? 'D' : 'F';
 
-          if (access_type == 'R' && isError != 0)
+          if (file_type == 'F' && access_type == 'R' && isError == 0)
           {
             // if open flags do not have O_DIRECTORY but path ends in '/' then it is a directory
-            if ((flags & O_DIRECTORY) == 0)
+            int pathLen = strlen(path);
+            if (pathLen > 0 && path[pathLen - 1] == '/')
             {
-              int pathLen = strlen(path);
-              if (pathLen > 0 && path[pathLen - 1] == '/')
-              {
-                file_type = 'D';
-              }
+              file_type = 'D';
             }
-            else if (rVal == -EISDIR)
+          }
+          else if (access_type == 'R' && isError != 0)
+          {
+            if (rVal == -EISDIR)
             {
               file_type = 'D';
             }
